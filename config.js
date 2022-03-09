89770113a641587b40fac6a301d0961da51a59e1
PantzCao <95122203+PantzCao@users.noreply.github.com>
2022-03-09 23:15:48 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Khuong';
+const giftUrl = 'https://www.facebook.com/dn.min.T6';
+const eventName = "Happy BirthDay";
+const titleCard = 'Tặng bạn iu';
+const contentCard = 'Chúc bạn iu sinh nhât tràn ngập niềm vui và những nụ cười. Chúc b sớm có em xinh tươi mới, kiếm nhiều tiền bao ae đi du lịch nhaaaaaaaa';
+
+
+const giftImage = 'Khuong.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
